23516ecc05b27cf58422b6fa2fb02478d411692e
caoli5288 <caoli5288@gmail.com>
2017-03-01 03:56:49 +0800

    Update Messenger.java 事实上牺牲性能换取使用方便的做法。也没有证据表明额外一次的列表创建会造成很大的负担。

--- src/main/java/com/mengcraft/util/Messenger.java
+++ src/main/java/com/mengcraft/util/Messenger.java
@@ -1,10 +1,11 @@
 package com.mengcraft.util;
 
+import lombok.val;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,7 +13,6 @@ import java.util.List;
  */
 public class Messenger {
 
-    private final static ArrayList<String> EMPTY_LIST = new ArrayList<>();
     private final static String PREFIX = "message.";
     private final Plugin plugin;
 
@@ -20,64 +20,63 @@ public class Messenger {
         this.plugin = plugin;
     }
 
-    public void send(CommandSender p, String path) {
-        send(p, path, "");
+    public void send(CommandSender receive, String path) {
+        send(receive, path, "");
     }
 
-    public void send(CommandSender p, String path, String def) {
-        sendMessage(p, find(path, def));
-    }
-
-    public void sendList(CommandSender p, String path, List<String> def) {
-        for (String line : findList(path, def)) {
-            sendMessage(p, line);
+    public void send(CommandSender receive, String path, String input) {
+        val found = find(path, input);
+        if (found.indexOf('\n') == -1) {
+            sendLine(receive, found);
+        } else {
+            for (String line : found.split("\n")) {
+                sendLine(receive, line);
+            }
         }
     }
 
-    public void sendList(CommandSender p, String path) {
-        sendList(p, path, EMPTY_LIST);
+    public void sendLine(CommandSender receive, String line) {
+        receive.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
     }
 
     public String find(String path) {
         return find(path, "");
     }
 
-    public List<String> findList(String path) {
-        return findList(path, EMPTY_LIST);
-    }
-
-    public List<String> findList(String path, List<String> def) {
-        List<String> found = plugin.getConfig().getStringList(with(path));
-        if (found.isEmpty()) {
-            if (!def.isEmpty()) {
-                plugin.getConfig().set(with(path), found = def);
-                plugin.saveConfig();
-            }
+    public String find(String path, String input) {
+        val found = plugin.getConfig().get(real(path), null);
+        if (found == null) {
+            if (input == null || input.isEmpty()) return path;
+            return find__(path, input);
+        } else if (found instanceof List) {
+            return find_((List) found);
         }
-        return found;
+        return found.toString();
     }
 
-    public String find(String path, String def) {
-        String found = plugin.getConfig().getString(with(path), "");
-        if (found.isEmpty()) {
-            if (!def.isEmpty()) {
-                plugin.getConfig().set(with(path), found = def);
-                plugin.saveConfig();
-            }
+    private String find_(List found) {
+        val b = new StringBuilder();
+        val i = found.iterator();
+        while (i.hasNext()) {
+            b.append(i.next());
+            if (i.hasNext()) b.append('\n');
         }
-        return found;
+        return b.toString();
     }
 
-    public void sendMessage(CommandSender p, String text) {
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', text));
+    private String find__(String path, String input) {
+        if (input.indexOf('\n') == -1) {
+            plugin.getConfig().set(real(path), input);
+            plugin.saveConfig();
+        } else {
+            plugin.getConfig().set(real(path), Arrays.asList(input.split("\n")));
+            plugin.saveConfig();
+        }
+        return input;
     }
 
-    private String with(String str) {
-        return PREFIX + str;
-    }
-
-    public Plugin getPlugin() {
-        return plugin;
+    private String real(String path) {
+        return PREFIX + path;
     }
 
 }
