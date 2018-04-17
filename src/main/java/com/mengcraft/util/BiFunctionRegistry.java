205b5fb97418b5293517c2565f72c3ddc458167e
caoli5288 <caoli5288@gmail.com>
2018-04-17 16:08:04 +0800

    Let FunctionRegistry.java, BiFunctionRegistry.java more generic

--- src/main/java/com/mengcraft/util/BiFunctionRegistry.java
+++ src/main/java/com/mengcraft/util/BiFunctionRegistry.java
@@ -7,9 +7,9 @@ import java.util.function.BiFunction;
 
 public class BiFunctionRegistry<T, U, R> {
 
-    private final Map<String, BiFunction<T, U, R>> all;
+    private final Map<Object, BiFunction<T, U, R>> all;
 
-    public BiFunctionRegistry(Map<String, BiFunction<T, U, R>> all) {
+    public BiFunctionRegistry(Map<Object, BiFunction<T, U, R>> all) {
         this.all = all;
     }
 
@@ -17,11 +17,11 @@ public class BiFunctionRegistry<T, U, R> {
         all = Maps.newHashMap();
     }
 
-    public void register(String key, BiFunction<T, U, R> function) {
+    public void register(Object key, BiFunction<T, U, R> function) {
         all.put(key, function);
     }
 
-    public R handle(String key, T i, U l) {
+    public R handle(Object key, T i, U l) {
         if (all.containsKey(key)) {
             return all.get(key).apply(i, l);
         }
--- src/main/java/com/mengcraft/util/FunctionRegistry.java
+++ src/main/java/com/mengcraft/util/FunctionRegistry.java
@@ -7,9 +7,9 @@ import java.util.function.Function;
 
 public class FunctionRegistry<T, R> {
 
-    private final Map<String, Function<T, R>> all;
+    private final Map<Object, Function<T, R>> all;
 
-    public FunctionRegistry(Map<String, Function<T, R>> all) {
+    public FunctionRegistry(Map<Object, Function<T, R>> all) {
         this.all = all;
     }
 
@@ -17,11 +17,11 @@ public class FunctionRegistry<T, R> {
         all = Maps.newHashMap();
     }
 
-    public void register(String key, Function<T, R> function) {
+    public void register(Object key, Function<T, R> function) {
         all.put(key, function);
     }
 
-    public R handle(String key, T input) {
+    public R handle(Object key, T input) {
         if (all.containsKey(key)) {
             return all.get(key).apply(input);
         }
