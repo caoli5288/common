f6e743cb9a72c3c616d6703dea14e8cc8224ddb0
caoli5288 <caoli5288@gmail.com>
2018-06-29 14:24:07 +0800

    Update Messenger.java

--- src/main/java/com/mengcraft/util/Messenger.java
+++ src/main/java/com/mengcraft/util/Messenger.java
@@ -1,66 +1,68 @@
 package com.mengcraft.util;
 
+import lombok.SneakyThrows;
 import lombok.val;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Created on 16-4-13.
  */
 public class Messenger {
 
-    private final static String PREFIX = "message.";
-    private final Plugin plugin;
+    private final File ymlFile;
+    private final YamlConfiguration yml;
 
     public Messenger(Plugin plugin) {
-        this.plugin = plugin;
+        ymlFile = new File(plugin.getDataFolder(), "message.yml");
+        yml = YamlConfiguration.loadConfiguration(ymlFile);
     }
 
-    private String path(String path) {
-        return PREFIX + path;
-    }
-
-    private String multi(Object found) {
-        val input = (List) found;
-        val b = new StringBuilder();
-        int size = input.size();
-        for (int l = 0; l < size; l++) {
-            if (l > 0) {
-                b.append('\n');
-            }
-            b.append(input.get(l));
-        }
-        return b.toString();
-    }
-
-    public String find(String path, String input) {
-        val found = plugin.getConfig().get(path(path), null);
-        if (found == null) {
-            if (input == null || input.isEmpty()) return path;
-            if (input.indexOf('\n') == -1) {
-                plugin.getConfig().set(path(path), input);
-                plugin.saveConfig();
-            } else {
-                plugin.getConfig().set(path(path), Arrays.asList(input.split("\n")));
-                plugin.saveConfig();
-            }
-            return input;
-        } else if (found instanceof List) {
-            return multi(found);
-        }
-        return found.toString();
+    @SneakyThrows
+    private void save() {
+        yml.save(ymlFile);
     }
 
     public String find(String path) {
         return find(path, "");
     }
 
-    public static void sendLine(CommandSender receive, String line) {
-        receive.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
+    public String find(String path, String input) {
+        val found = yml.get(path, null);
+        if (found == null) {
+            if (input == null) {
+                return path;
+            }
+            if (input.indexOf('\n') == -1) {
+                yml.set(path, input);
+            } else {
+                yml.set(path, Arrays.asList(input.split("\n")));
+            }
+            save();
+            return input;
+        } else if (found instanceof List) {
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Object line : (List) found) {
+                joiner.add(String.valueOf(line));
+            }
+            return joiner.toString();
+        }
+        return found.toString();
+    }
+
+    public void send(CommandSender receive, String path) {
+        send(receive, path, "");
+    }
+
+    public void send(CommandSender receive, String path, String input) {
+        sendMessage(receive, find(path, input));
     }
 
     public static void sendMessage(CommandSender receive, String message) {
@@ -73,12 +75,8 @@ public class Messenger {
         }
     }
 
-    public void send(CommandSender receive, String path, String input) {
-        sendMessage(receive, find(path, input));
-    }
-
-    public void send(CommandSender receive, String path) {
-        send(receive, path, "");
+    public static void sendLine(CommandSender receive, String line) {
+        receive.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
     }
 
 }
