4e25016030d845dd784e65a94de016b73f2983d8
caoli5288 <caoli5288@gmail.com>
2024-07-11 10:48:52 +0800

    fix(GuiBuilder): handle non-collection lore as string in Element

--- src/main/java/com/mengcraft/util/GuiBuilderSpec.java
+++ src/main/java/com/mengcraft/util/GuiBuilderSpec.java
@@ -3,11 +3,15 @@ package com.mengcraft.util;
 import lombok.Data;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,30 +39,36 @@ public class GuiBuilderSpec {
     public static class Element {
 
         private String symbol;
-        private int id;
+        private Material type;
         private int data;
         private Map<String, ?> metadata;
         private List<String> commands = new ArrayList<>();
 
         public GuiBuilder.Button apply(Player player) {
-            ItemStack item = new ItemStack(id, 1, (short) data);
+            ItemStack item = new ItemStack(type, 1, (short) data);
             if (metadata != null) {
                 ItemMeta meta = item.getItemMeta();
                 if (metadata.containsKey("name")) {
                     meta.setDisplayName(PlaceholderAPI.setPlaceholders(player, metadata.get("name").toString()));
                 }
                 if (metadata.containsKey("lore")) {
-                    List<String> lore = (List<String>) metadata.get("lore");
-                    meta.setLore(lore.stream().map(line -> PlaceholderAPI.setPlaceholders(player, line)).collect(Collectors.toList()));
+                    Object lore = metadata.get("lore");
+                    if (lore instanceof Collection) {
+                        meta.setLore(((Collection<String>) lore).stream().map(line -> PlaceholderAPI.setPlaceholders(player, line)).collect(Collectors.toList()));
+                    } else {
+                        String lines = PlaceholderAPI.setPlaceholders(player, lore.toString());
+                        meta.setLore(Arrays.asList(lines.split("\n")));
+                    }
                 }
                 item.setItemMeta(meta);
             }
             return new GuiBuilder.Button(item, e -> {
                 if (!commands.isEmpty()) {
                     Player p = (Player) e.getWhoClicked();
+                    ConsoleCommandSender console = Bukkit.getConsoleSender();
                     for (String command : commands) {
                         String res = PlaceholderAPI.setPlaceholders(p, command);
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), res);
+                        Bukkit.dispatchCommand(console, res);
                     }
                 }
             });
