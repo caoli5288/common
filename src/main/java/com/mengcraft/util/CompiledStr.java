293d43d2c1811db8c9435b055c9f7e77343c5b4a
caoli5288 <caoli5288@gmail.com>
2026-05-03 13:38:58 +0800

    refactor(util): 重构CompiledStr类以改进代码质量和性能

--- src/main/java/com/mengcraft/util/CompiledStr.java
+++ src/main/java/com/mengcraft/util/CompiledStr.java
@@ -1,54 +1,56 @@
 package com.mengcraft.util;
 
+import com.google.common.collect.Lists;
 import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.clip.placeholderapi.expansion.manager.LocalExpansionManager;
 import org.bukkit.OfflinePlayer;
 
-import java.util.ArrayList;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CompiledStr implements Function<OfflinePlayer, String> {
 
-    private final String template;
-    private String[] texts;
-    private PlaceholderExpansion[] expansions;
-    private String[] commands;
+    private final @NotNull String template;
+    private @NotNull String[] texts;
+    private @NotNull PlaceholderExpansion[] expansions;
 
-    private CompiledStr(String template) {
+    private CompiledStr(@NotNull String template) {
         this.template = template;
     }
 
-    public static CompiledStr compile(String text) {
+    public static @NotNull CompiledStr compile(@NotNull String text) {
         return compile(Style.PERCENT, text);
     }
 
-    public static CompiledStr compile(Style style, String text) {
+    public static @NotNull CompiledStr compile(@NotNull Style style, @NotNull String text) {
         CompiledStr let = new CompiledStr(text);
         let.compile(style);
         return let;
     }
 
     @Override
-    public String apply(OfflinePlayer player) {
+    public @NotNull String apply(@Nullable OfflinePlayer player) {
         StringBuilder sb = new StringBuilder(template.length());
         for (int i = 0; i < texts.length; i++) {
             PlaceholderExpansion it = expansions[i];
             if (it == null) {
                 sb.append(texts[i]);
             } else {
-                sb.append(it.onRequest(player, commands[i]));
+                sb.append(it.onRequest(player, texts[i]));
             }
         }
         return sb.toString();
     }
 
-    private void compile(Style style) {
-        ArrayList<String> textList = new ArrayList<>();
-        ArrayList<PlaceholderExpansion> expansionList = new ArrayList<>();
-        ArrayList<String> commandList = new ArrayList<>();
+    private void compile(@NotNull Style style) {
+        List<String> allTexts = Lists.newArrayList();
+        List<PlaceholderExpansion> allExpansions = Lists.newArrayList();
         LocalExpansionManager lem = PlaceholderAPIPlugin.getInstance().getLocalExpansionManager();
         Matcher mat = style.matcher(template);
         int seg = 0;
@@ -56,29 +58,25 @@ public class CompiledStr implements Function<OfflinePlayer, String> {
             int f = mat.start();
             if (seg != f) {
                 String segment = template.substring(seg, f);
-                textList.add(segment);
-                expansionList.add(null);
-                commandList.add(null);
+                allTexts.add(segment);
+                allExpansions.add(null);
             }
             seg = mat.end();
             String exp = mat.group(1);
             String command = mat.group(2);
             PlaceholderExpansion expansion = lem.getExpansion(exp);
             if (expansion != null) {
-                textList.add(null);
-                expansionList.add(expansion);
-                commandList.add(command);
+                allTexts.add(command);
+                allExpansions.add(expansion);
             }
         }
         if (seg != template.length()) {
             String segment = template.substring(seg);
-            textList.add(segment);
-            expansionList.add(null);
-            commandList.add(null);
+            allTexts.add(segment);
+            allExpansions.add(null);
         }
-        texts = textList.toArray(new String[0]);
-        expansions = expansionList.toArray(new PlaceholderExpansion[0]);
-        commands = commandList.toArray(new String[0]);
+        texts = allTexts.toArray(new String[0]);
+        expansions = allExpansions.toArray(new PlaceholderExpansion[0]);
     }
 
     public enum Style {
@@ -87,11 +85,11 @@ public class CompiledStr implements Function<OfflinePlayer, String> {
 
         private final Pattern pattern;
 
-        Style(Pattern pattern) {
+        Style(@NotNull Pattern pattern) {
             this.pattern = pattern;
         }
 
-        public Matcher matcher(String template) {
+        public @NotNull Matcher matcher(@NotNull String template) {
             return pattern.matcher(template);
         }
     }
